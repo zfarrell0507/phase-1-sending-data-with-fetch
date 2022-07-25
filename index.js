6870dc900f2d42db34b38f22437c289f3ec235ae
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-25 17:36:17 -0400

    commit tree 22ccfa6aeafc7d6b46dc48aad7a934005e2b93e2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "name": `${name}`,
+                "email": `${email}`
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            document.body.innerHTML = data["id"]
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message
+        })
+}
+submitData("Zachary", "zfarrell0507@gmail.com")
